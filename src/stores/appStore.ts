520b1eb72ec6c8c2a3d3b8f515456364b27334c9
Harshit6396 <91064861+Harshit6396@users.noreply.github.com>
2024-09-04 11:16:11 +0530

    Added pinia

---
+++ src/stores/appStore.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const useAppStore = defineStore({
+  id: "appStore",
+  state: () => ({
+    //Add State Variables    
+  }),
+  getters: {
+    
+  },
+
+  actions: {
+    //Add funtions
+  }
+});
