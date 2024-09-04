75adf8e237a786fa9d46b6d13ad420f1b0878281
Harshit6396 <91064861+Harshit6396@users.noreply.github.com>
2024-09-04 11:11:12 +0530

    Added build files

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/sq-headshot-tool-app.min.js`,
+        chunkFileNames: `assets/sq-headshot-tool-common.min.js`,
+        assetFileNames: `assets/sq-headshot-tool-app.min.[ext]`,
+      },
+    },
+  },
 })
