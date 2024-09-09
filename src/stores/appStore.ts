6bec58be10dbac01f44cbbe49bb425c6fd0339ef
Aniket Bhatnagar <101235638+Aniket8448@users.noreply.github.com>
2024-09-09 18:11:19 +0530

    Wip slider

--- src/stores/appStore.ts
+++ src/stores/appStore.ts
@@ -42,7 +42,7 @@ export const useAppStore = defineStore({
         const canvas = document.createElement('canvas');
         canvas.width = img.width;
         canvas.height = img.height;
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d') as any;
         ctx?.drawImage(img, 0, 0);
 
         // Load the BodyPix model
