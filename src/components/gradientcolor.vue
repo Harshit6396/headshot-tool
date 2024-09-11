b9f709e96242b6534afc3c105643b45372d11811
Harshit6396 <91064861+Harshit6396@users.noreply.github.com>
2024-09-11 15:03:08 +0530

    gradient color

---
+++ src/components/gradientcolor.vue
@@ -0,0 +1,358 @@
+<template>
+  <div
+    class="grid grid-cols-8 gap-2 bg-gray-100 p-2 rounded-md border border-gray-200"
+  >
+    <button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(255, 255, 255);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(255, 255, 255) 0%,
+          rgb(205, 205, 205) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(0, 0, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(0, 0, 0) 0%,
+          rgb(205, 205, 205) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(238, 9, 121);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(238, 9, 121) 0%,
+          rgb(255, 106, 0) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(228, 58, 21);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(228, 58, 21) 0%,
+          rgb(230, 82, 69) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(238, 156, 167);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(238, 156, 167) 0%,
+          rgb(255, 221, 225) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(236, 0, 140);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(236, 0, 140) 0%,
+          rgb(252, 103, 103) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(249, 83, 198);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(249, 83, 198) 0%,
+          rgb(185, 29, 115) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(204, 43, 94);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(204, 43, 94) 0%,
+          rgb(117, 58, 136) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(255, 0, 153);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(255, 0, 153) 0%,
+          rgb(73, 50, 64) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(204, 43, 94);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(204, 43, 94) 0%,
+          rgb(117, 58, 136) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(236, 0, 140);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(236, 0, 140) 0%,
+          rgb(252, 103, 103) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(142, 45, 226);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(142, 45, 226) 0%,
+          rgb(74, 0, 224) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(247, 255, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(247, 255, 0) 0%,
+          rgb(219, 54, 164) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(230, 92, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(230, 92, 0) 0%,
+          rgb(249, 212, 35) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(255, 226, 89);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(255, 226, 89) 0%,
+          rgb(255, 167, 81) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(194, 21, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(194, 21, 0) 0%,
+          rgb(255, 197, 0) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(230, 92, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(230, 92, 0) 0%,
+          rgb(249, 212, 35) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(255, 224, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(255, 224, 0) 0%,
+          rgb(121, 159, 12) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(121, 159, 12);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(121, 159, 12) 0%,
+          rgb(172, 187, 120) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(0, 70, 127);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(0, 70, 127) 0%,
+          rgb(165, 204, 130) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(65, 77, 11);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(65, 77, 11) 0%,
+          rgb(114, 122, 23) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(28, 216, 210);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(28, 216, 210) 0%,
+          rgb(147, 237, 199) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(61, 126, 170);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(61, 126, 170) 0%,
+          rgb(255, 228, 122) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(218, 226, 248);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(218, 226, 248) 0%,
+          rgb(214, 164, 164) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(252, 53, 76);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(252, 53, 76) 0%,
+          rgb(10, 191, 188) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(33, 147, 176);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(33, 147, 176) 0%,
+          rgb(109, 213, 237) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(18, 194, 233);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(18, 194, 233) 0%,
+          rgb(196, 113, 237) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(84, 51, 255);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(84, 51, 255) 0%,
+          rgb(165, 254, 203) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(20, 136, 204);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(20, 136, 204) 0%,
+          rgb(43, 50, 178) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(185, 43, 39);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(185, 43, 39) 0%,
+          rgb(21, 101, 192) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(142, 45, 226);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(142, 45, 226) 0%,
+          rgb(74, 0, 224) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(151, 150, 240);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(151, 150, 240) 0%,
+          rgb(251, 199, 212) 100%
+        );
+      "
+    ></button>
+  </div>
+</template>
---
+++ src/components/solidcolor.vue
@@ -0,0 +1,358 @@
+<template>
+  <div
+    class="grid grid-cols-8 gap-2 bg-gray-100 p-2 rounded-md border border-gray-200"
+  >
+    <button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(255, 255, 255);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(255, 255, 255) 0%,
+          rgb(255, 255, 255) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(0, 0, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(0, 0, 0) 0%,
+          rgb(0, 0, 0) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(238, 9, 121);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(238, 9, 121) 0%,
+          rgb(238, 9, 121) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(228, 58, 21);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(228, 58, 21) 0%,
+          rgb(228, 58, 21) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(238, 156, 167);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(238, 156, 167) 0%,
+          rgb(238, 156, 167) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(236, 0, 140);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(236, 0, 140) 0%,
+          rgb(236, 0, 140) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(249, 83, 198);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(249, 83, 198) 0%,
+          rgb(249, 83, 198) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(204, 43, 94);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(204, 43, 94) 0%,
+          rgb(204, 43, 94) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(255, 0, 153);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(255, 0, 153) 0%,
+          rgb(255, 0, 153) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(204, 43, 94);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(204, 43, 94) 0%,
+          rgb(204, 43, 94) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(236, 0, 140);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(236, 0, 140) 0%,
+          rgb(236, 0, 140) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(142, 45, 226);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(142, 45, 226) 0%,
+          rgb(142, 45, 226) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(247, 255, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(247, 255, 0) 0%,
+          rgb(247, 255, 0) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(230, 92, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(230, 92, 0) 0%,
+          rgb(230, 92, 0) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(255, 226, 89);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(255, 226, 89) 0%,
+          rgb(255, 226, 89) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(194, 21, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(194, 21, 0) 0%,
+          rgb(194, 21, 0) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(230, 92, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(230, 92, 0) 0%,
+          rgb(230, 92, 0) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(255, 224, 0);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(255, 224, 0) 0%,
+          rgb(255, 224, 0) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(121, 159, 12);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(121, 159, 12) 0%,
+          rgb(121, 159, 12) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(0, 70, 127);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(0, 70, 127) 0%,
+          rgb(0, 70, 127) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(65, 77, 11);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(65, 77, 11) 0%,
+          rgb(65, 77, 11) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(28, 216, 210);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(28, 216, 210) 0%,
+          rgb(28, 216, 210) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(61, 126, 170);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(61, 126, 170) 0%,
+          rgb(61, 126, 170) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(218, 226, 248);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(218, 226, 248) 0%,
+          rgb(218, 226, 248) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(252, 53, 76);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(252, 53, 76) 0%,
+          rgb(252, 53, 76) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(33, 147, 176);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(33, 147, 176) 0%,
+          rgb(33, 147, 176) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(18, 194, 233);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(18, 194, 233) 0%,
+          rgb(18, 194, 233) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(84, 51, 255);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(84, 51, 255) 0%,
+          rgb(84, 51, 255) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(20, 136, 204);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(20, 136, 204) 0%,
+          rgb(20, 136, 204) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(185, 43, 39);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(185, 43, 39) 0%,
+          rgb(185, 43, 39) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(142, 45, 226);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(142, 45, 226) 0%,
+          rgb(142, 45, 226) 100%
+        );
+      "
+    ></button
+    ><button
+      class="border border-gray-100 w-full h-[32px] rounded-md"
+      style="
+        background-color: rgb(151, 150, 240);
+        background-image: -webkit-linear-gradient(
+          45deg,
+          rgb(151, 150, 240) 0%,
+          rgb(151, 150, 240) 100%
+        );
+      "
+    ></button>
+  </div>
+</template>
